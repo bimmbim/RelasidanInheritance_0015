42f92ee9a42aee29b8d03f7b7e8692cc1d9969ee
bimmbim <bimabaraja76@gmail.com>
2025-05-26 11:35:54 +0700

    Tambah kelas dasar Ibu dan Anak

---
+++ agregasi/agregasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main() {
+    ibu* varIbu1 = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+
+
+
+}
